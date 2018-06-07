d9b4a052702847c40e481bfcca9ffcdc5a08e6bb
Francois Lecerf <flecerf2511@gmail.com>
2018-06-08 00:43:11 +0200

    Transition done

--- projet/js/france_names.js
+++ projet/js/france_names.js
@@ -96,83 +96,13 @@ d3.tsv("data/nat2016.txt", rowNatConverter, function(error, data) {
         return d.preusuel;
       });
 
-    var groupBubbles = node.exit()
-    .remove()
-    .transition().duration(1000)
-    .attr("r", 0);
-    
-    var groupBubbles = node
-      .enter()
-      .append("g")
-      .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+    // remove a bubble
+    node.exit()
+      .transition().duration(1000)
+      .attr("r", 0)
+      .remove();
 
-    //var groupBubbles = node
-    //  .update()
-    //  .append("g")
-    //  .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
-
-    // only for new circles
-    var bubbles = groupBubbles.append("circle")
-      .style("fill", function(d) {
-        return color(d.angle = Math.atan2(d.y, d.x));
-      })
-      .attr("cx", function(d) {
-        return Math.cos(d.angle) * (w / Math.SQRT2 + 30);
-      })
-      .attr("cy", function(d) {
-        return Math.sin(d.angle) * (h / Math.SQRT2 + 30);
-      })
-      //.attr("r", function(d) {
-      //  return d.r - 0.25;
-      .attr("r", function(d) {
-        return d.r //- 0.25
-      })
-      .transition()
-      .ease(d3.easeCubicOut)
-      .delay(function(d) {
-        return Math.sqrt(d.x * d.x + d.y * d.y) * 10;
-      })
-      .duration(1000)
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      });
-
-
-    //title
-    groupBubbles.append("title")
-      //  .attr("text-anchor", "middle")
-      .text(function(d) {
-        return d.preusuel + " : " + d.nombre;
-      })
-
-    groupBubbles.append("text")
-      //.attr("font", "40px sans-serif")
-      //.attr("font-weight", "bold")
-      .attr("x", function(d) {
-        return d.x;
-      })
-      .attr("y", function(d) {
-        return d.y;
-      })
-      //.attr("textlength", function(d) {
-      //  // quick and dirty : to refactor
-      //  return Math.round(d.r) + 'px';
-      //})
-      //.attr("lengthAdjust", "spacingAndGlyphs")
-      .style("text-anchor", "middle")
-
-      .style("font-size", function(d) {
-        // quick and dirty : to refactor
-        return Math.round(d.r / 3) + 'px';
-      })
-      .text(function(d) {
-        return d.preusuel;
-      });
-
-
+    // update a bubble
     node.select("circle")
       .transition().duration(1000)
       .attr("r", function(d) {
@@ -197,5 +127,74 @@ d3.tsv("data/nat2016.txt", rowNatConverter, function(error, data) {
         // quick and dirty : to refactor
         return Math.round(d.r / 3) + 'px';
       });
+
+    // add a new bubble (none if modified)
+    var groupBubbles = node
+      .enter()
+      .append("g")
+      .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+
+
+    // only for new circles
+    groupBubbles.append("circle")
+      .style("fill", function(d) {
+        return color(d.angle = Math.atan2(d.y, d.x));
+      })
+      .attr("cx", function(d) {
+        return Math.cos(d.angle) * (w / Math.SQRT2 + 30);
+      })
+      .attr("cy", function(d) {
+        return Math.sin(d.angle) * (h / Math.SQRT2 + 30);
+      })
+      .attr("r", function(d) {
+        return d.r //- 0.25
+      })
+      .transition()
+      .ease(d3.easeCubicOut)
+      .delay(function(d) {
+        return Math.sqrt(d.x * d.x + d.y * d.y) * 10;
+      })
+      .duration(1000)
+      .attr("cx", function(d) {
+        return d.x;
+      })
+      .attr("cy", function(d) {
+        return d.y;
+      });
+
+    //title
+    groupBubbles.append("title")
+      .text(function(d) {
+        return d.preusuel + " : " + d.nombre;
+      })
+
+    groupBubbles.append("text")
+      .attr("x", function(d) {
+        return Math.cos(d.angle) * (w / Math.SQRT2 + 30);
+      })
+      .attr("y", function(d) {
+        return Math.sin(d.angle) * (h / Math.SQRT2 + 30);
+      })
+      .style("text-anchor", "middle")
+      .style("font-size", function(d) {
+        // quick and dirty : to refactor
+        return Math.round(d.r / 3) + 'px';
+      })
+      .text(function(d) {
+        return d.preusuel;
+      })
+      .transition()
+      .ease(d3.easeCubicOut)
+      .delay(function(d) {
+        return Math.sqrt(d.x * d.x + d.y * d.y) * 10;
+      })
+      .duration(1000)
+      .attr("x", function(d) {
+        return d.x;
+      })
+      .attr("y", function(d) {
+        return d.y;
+      });
+
   }
 });
